e9b1c1f7fd4804998fe810bf7affd094c16b7fa4
= <=>
2015-10-03 17:25:19 +0100

    Separation: Stage 2

--- Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicSeparation.cs
+++ Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicSeparation.cs
@@ -1,4 +1,8 @@
-﻿namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+﻿using Assets.Scripts.IAJ.Unity.Util;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
 {
     public class DynamicSeparation : DynamicMovement
     {
@@ -15,6 +19,26 @@
         {
             var output = new MovementOutput();
 
+            foreach (DynamicCharacter boid in this.Flock)
+            {
+                if (boid.KinematicData != this.Character)
+                {
+                    Vector3 direction = this.Character.position - boid.KinematicData.position;
+                    float distance = direction.sqrMagnitude;
+                    if (distance < Radius)
+                    {
+                        float separationStrength = Math.Min(SeparationFactor / (distance * distance), MaxAcceleration);
+                        direction.Normalize();
+                        output.linear += direction * separationStrength;
+                    }
+                }
+            }
+
+            if (output.linear.sqrMagnitude > MaxAcceleration)
+                {
+                    output.linear.Normalize();
+                    output.linear *= MaxAcceleration;
+                }        
           
             return output;
         }
