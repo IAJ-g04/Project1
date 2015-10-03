41a23cd5d9682e14c37ee1f926344fc56ece780c
Kaguedes <tiagoasc.69@hotmail.com>
2015-10-03 17:00:05 +0100

    -.-''

---
+++ Assets/Scripts/IAJ.Unity/Movement/DynamicMovement/DynamicCohesion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.Movement.DynamicMovement
+{
+    public class DynamicCohesion : DynamicArrive
+    {
+        public override string Name
+        {
+            get { return "Cohesion"; }
+        }
+
+        public float radius;
+
+        public float FanAngle;
+
+        public override MovementOutput GetMovement()
+        {
+
+            Vector3 massCenter = new Vector3();
+            float closeBoids = 0.0f;
+            foreach(GameObject boid in Flock)
+            {
+              //  if(this.Character != boid.GetComponent)
+            }
+
+
+            var output = new MovementOutput();
+
+            output.linear = this.Target.position - this.Character.position;
+
+            if (output.linear.sqrMagnitude > 0)
+            {
+                output.linear.Normalize();
+                output.linear *= this.MaxAcceleration;
+            }
+
+            return output;
+        }
+    }
+}
